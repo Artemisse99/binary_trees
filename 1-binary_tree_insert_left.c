09bdaa61d08c57d3654cb1d175641445126d665a
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-23 21:54:33 -0500

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,35 +6,37 @@
 * @value: value to store in the new node.
 * Return: pointer to the new node, or NULL on failure.
 */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *child_left;
-	binary_tree_t *tmp;
 
-	if (parent == NULL)
-		return (NULL);
+binary_tree_t *child_left;
+binary_tree_t *tmp;
 
-	child_left = malloc(sizeof(binary_tree_t));
-	if (child_left == NULL)
-		return (NULL);
+if (parent == NULL)
+return (NULL);
 
-	child_left->left = NULL;
-	child_left->right = NULL;
-	child_left->n = value;
-	child_left->parent = parent;
+child_left = malloc(sizeof(binary_tree_t));
+if (child_left == NULL)
+return (NULL);
 
-	if (parent->left == NULL)
-		parent->left = child_left;
+child_left->left = NULL;
+child_left->right = NULL;
+child_left->n = value;
+child_left->parent = parent;
 
-	else
-	{
-		tmp = parent->left;
-		parent->left = child_left;
-		child_left->left = tmp;
-		tmp->left = NULL;
-		tmp->right = NULL;
-		tmp->parent = child_left;
+if (parent->left == NULL)
+parent->left = child_left;
 
-	}
-	return (child_left);
+else
+{
+tmp = parent->left;
+parent->left = child_left;
+child_left->left = tmp;
+tmp->left = NULL;
+tmp->right = NULL;
+tmp->parent = child_left;
+
+}
+return (child_left);
 }
