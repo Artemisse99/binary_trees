ea725d28a1db3a8683513b936d678d1cf5ba27a9
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-24 10:59:31 -0500

    Postorder

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,20 +1,18 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_is_leaf - creates a binary tree node.
+* binary_tree_postorder - function that goes through a binary tree
+* using post-order traversal
 * @tree: pointer to the parent node of the node to create.
 * @func: pointer to the parent node of the node to create.
 * Return: pointer to the new node, or NULL on failure.
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    
-    if (!tree || !func)
-        return;
-    
-    /* then recur on left subtree */
-    binary_tree_postorder(tree->left, func);
-    /* now recur on right subtree */
-    binary_tree_postorder(tree->right, func);
-    func(tree->n);
+
+if (!tree || !func)
+return;
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+func(tree->n);
 }
