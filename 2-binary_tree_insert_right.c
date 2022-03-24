166189a86f9edc008ab753d41d1bc6696d64cfb4
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-23 21:58:59 -0500

    Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,33 +8,33 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-   binary_tree_t *child_right;
-	binary_tree_t *tmp;
+binary_tree_t *child_right;
+binary_tree_t *tmp;
 
-	if (parent == NULL)
-		return (NULL);
+if (parent == NULL)
+return (NULL);
 
-	child_right = malloc(sizeof(binary_tree_t));
-	if (child_right == NULL)
-		return (NULL);
+child_right = malloc(sizeof(binary_tree_t));
+if (child_right == NULL)
+return (NULL);
 
-	child_right->left = NULL;
-	child_right->right = NULL;
-	child_right->n = value;
-	child_right->parent = parent;
+child_right->left = NULL;
+child_right->right = NULL;
+child_right->n = value;
+child_right->parent = parent;
 
-	if (parent->right == NULL)
-		parent->right = child_right;
+if (parent->right == NULL)
+parent->right = child_right;
 
-	else
-	{
-		tmp = parent->right;
-		parent->right = child_right;
-		child_right->right = tmp;
-		tmp->left = NULL;
-		tmp->right = NULL;
-		tmp->parent = child_right;
+else
+{
+tmp = parent->right;
+parent->right = child_right;
+child_right->right = tmp;
+tmp->left = NULL;
+tmp->right = NULL;
+tmp->parent = child_right;
 
-	}
-	return (child_right);
+}
+return (child_right);
 }
