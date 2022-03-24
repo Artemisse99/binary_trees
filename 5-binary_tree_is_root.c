631d3b244856908c4157e4e1deef286e27af6022
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-24 10:23:38 -0500

    is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,14 +1,16 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_is_leaf - creates a binary tree node.
+* binary_tree_is_root - creates a binary tree node.
 * @node: pointer to the parent node of the node to create.
 * Return: pointer to the new node, or NULL on failure.
 */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-if(!node)
+if (node == null)
 return(0)
-if(node->left!=NULL && node->right!=NULL) return 1;
-else return 0;
+if (node->left != NULL && node->right != NULL)
+return (1);
+else
+return (0);
 }
