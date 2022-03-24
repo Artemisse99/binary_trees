2f4c0229d61405622f17c310d6f06904966f2b1e
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-24 10:37:36 -0500

    Is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,9 +8,8 @@
 int binary_tree_is_root(const binary_tree_t *node)
 {
 if (node == NULL)
-return(0);
-if (node->parent != NULL)
+return (0);
+if (node && !node->parent)
 return (1);
-else
 return (0);
 }
