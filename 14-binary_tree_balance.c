281c64cd9a30238a30d0ac095c62762c1ee72459
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-24 11:22:35 -0500

    Balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,7 +29,7 @@ return (left);
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-if (!tree)
+if (tree == NULL)
 return (0);
 return ((int)binary_tree_height(tree->left) -
 (int)binary_tree_height(tree->right));
